e24a05b92ac3b0a14636e067ff4da18538c903ae
casept <davids.paskevics@gmail.com>
2019-09-16 09:13:38 +0200

    Add more robust error handling to example

--- examples/simple_forward.rs
+++ examples/simple_forward.rs
@@ -25,13 +25,17 @@ fn main() {
 
     // Process each connection
     for connection in server {
-        let conn = connection.unwrap();
+        let conn;
+        match connection {
+            Ok(val) => conn = val,
+            Err(e) => {
+                eprintln!("Failed to handle client connection: {}", e);
+                continue;
+            }
+        }
 
         // Start a new thread to process the connection
-        let _ = thread::Builder::new()
-            .name("SOCKSMain".to_string())
-            .stack_size(10 * 1024)
-            .spawn(move || {
+        thread::spawn(move || {
                 // If you want to disallow certain connections, perform the filtering here, then
                 // call conn.report_connection_not_allowed() to tell the client about a violation.
 
@@ -54,31 +58,31 @@ fn main() {
                             .set_write_timeout(Some(time::Duration::from_secs(1)))
                             .unwrap();
                         // If successful, tell the client to expect data to start being relayed
-                        let ready_conn = conn.report_success();
+                        let ready_conn = conn.report_success().unwrap();
 
                         // Start relaying data between the two streams
                         println!("Starting to proxy data!");
                         // Spawn 2 threads to continuously copy on both directions
                         // This is not very efficient, but good enough for a demo.
                         // We need to clone the stream here because both the reading and writing threads need a mutable handle
-                        let mut client_stream_1 = ready_conn.unwrap().get_stream();
+                        let mut client_stream_1 = ready_conn.get_stream();
                         let mut client_stream_2 = client_stream_1.try_clone().unwrap();
                         let mut server_stream_1 = remote_stream;
                         let mut server_stream_2 = server_stream_1.try_clone().unwrap();
                         // Client => Server
                         let _ = thread::Builder::new()
                             .stack_size(10 * 1024)
-                            .spawn(move || io::copy(&mut client_stream_1, &mut server_stream_1));
+                            .spawn(move || io::copy(&mut client_stream_1, &mut server_stream_1).ignore());
                         // Server => Client
                         let _ = thread::Builder::new()
                             .stack_size(10 * 1024)
-                            .spawn(move || io::copy(&mut server_stream_2, &mut client_stream_2));
+                            .spawn(move || io::copy(&mut server_stream_2, &mut client_stream_2).ignore());
                     }
                     // If that fails, tell the client
                     // Note that in a real-world program you should look at the error closely
                     // and invoke the most appropriate error reporting method.
                     Err(err) => {
-                        println!("Failed to reach destination: {}", err);
+                        eprintln!("Failed to reach destination: {}", err);
                         match err.kind() {
                             io::ErrorKind::ConnectionRefused => conn.report_connection_refused().ignore(),
                             io::ErrorKind::NotFound => conn.report_destination_unreachable().ignore(),
